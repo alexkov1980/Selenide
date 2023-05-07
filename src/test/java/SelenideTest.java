c0e51cb52882d2ecf1f34a870337571e81a102cd
Alexey Kovalev <amkovalyov@yandex.ru>
2023-05-07 21:29:31 +0300

    test

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,2 +1,26 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
 public class SelenideTest {
+    @Test
+    void shouldRegisterCard() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Ковалев Алексей");
+        $("[data-test-id=phone] input").setValue("+79123456789");
+        $("[data-test-id=agreement]").click();
+        $x("//*[text()=\"Забронировать\"]").click();
+        $("[data-test-id=notification]").shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + date), Duration.ofSeconds(15));
+    }
 }
